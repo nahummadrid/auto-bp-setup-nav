5a2993ecb3482c69f563fa703a6308df4ba62b8b
nahummadrid <nahummadrid@gmail.com>
2019-06-02 20:28:56 -0400

    Update auto-bp-screens.php

--- auto-bp-screens.php
+++ auto-bp-screens.php
@@ -21,82 +21,62 @@ $post_types = get_post_types( $args, $output, $operator );
 
 foreach ( $post_types  as $post_type ) {
 
+$user_post_count = mdlr_count_cpt_post($post_type->name);
+$count_class  = ( 0 === $user_post_count ) ? 'no-count' : 'count';
+
+    if($user_post_count > 0) {
       bp_core_new_nav_item(
       array(
-      'name' => $post_type->labels->name,
+      'name' => sprintf( __( '%s <span class="%s">%s</span>', 'mdlr' ),  $post_type->labels->name,  esc_attr( $count_class ), number_format_i18n( $user_post_count ) ),
       'slug' =>  strtolower($post_type->labels->name), 
       'position' => 10, 
       'screen_function' => 'mdlr_bp_submenu_redirect_' .$post_type->name,
               
       )
       );
-    
-
-  } //foreach posttype
-
-}
-
-/*/////////////////////////////////////////////*/
-/* Setup Sub Nav Menus */
-/*/////////////////////////////////////////////*/
-
-add_action('bp_setup_nav', 'mb_bp_user_submenus',10);
-
-function mb_bp_user_submenus() {
-global $bp, $wpdb;
-
-$userid = bp_displayed_user_id();
-
-//get the public post types //////////////////////////////////////
-$args = array(
-   'public'   => true,
-   '_builtin' => false
-);
-
-$output = 'objects'; // names or objects, note names is the default
-$operator = 'and'; // 'and' or 'or'
-
-$post_types = get_post_types( $args, $output, $operator ); 
-//get the public post types //////////////////////////////////////
-foreach ( $post_types  as $post_type ) {
+  
 
 $parent_slug = strtolower($post_type->labels->name);
 
-if(bp_is_user()) { //needed this check for some reason but I now forgot what! derp
+    if(bp_is_user()) { //needed this check for some reason but I now forgot what! derp
+      bp_core_new_subnav_item( array( 
+      'name' => sprintf( __( '%s <span class="%s">%s</span>', 'mdlr' ), $post_type->labels->name, esc_attr( $count_class ), number_format_i18n( $user_post_count ) ),
+      'slug' => 'added', 
+      'parent_url' => $bp->displayed_user->domain . $parent_slug.'/', 
+      'parent_slug' => $parent_slug, 
+      'position' => 20,
+      'screen_function' => 'mb_author_'.$post_type->name, //the function is declared below
+      ) 
+    ); 
+      }//if user has posts
 
- bp_core_new_subnav_item( array( 
- 'name' => $post_type->labels->name,
- 'slug' => 'added', 
- 'parent_url' => $bp->displayed_user->domain . $parent_slug.'/', 
- 'parent_slug' => $parent_slug, 
- 'position' => 20,
- 'screen_function' => 'mb_author_'.$post_type->name, //the function is declared below
-    ) 
-  ); 
-
-} //bpisuser
-
-
-if(bp_is_user()) {  //again needed this check for some reason but I now forgot what! 
-
-   bp_core_new_subnav_item( array( 
-  'name' => $favorite_name,
-  'slug' => 'favorites', 
-   'parent_url' => $bp->displayed_user->domain . $parent_slug.'/', 
-  'parent_slug' => $parent_slug, 
-  'position' =>30,
-  'screen_function' => 'mb_author_'.$post_type->name.'_likes' //the function is declared below
-    ) 
-  ); 
-
- }//bpisuser
-
-}//foreach
-  
-}
+  } //bpisuser
 
 
-//get post counts per cpt
+  //Favorites //////////////////////////////////////
+  $user_favorites = (class_exists('favorites')) ? get_user_favorites($userid) : (array('1')); //just go with it
+  $favorite_args = array( 'post_type' => $post_type->name, 'numberposts' => -1, 'post__in'=> $user_favorites );
+  $favorite_posts = get_posts($favorite_args);
+  $favorite_count = (!empty($favorite)) ? count($favorite_posts) : '0';
+  $favorite_class = ( 0 === $playcount ) ? 'no-count' : 'count';
+  $favorite_name = sprintf( __( 'Favorites <span class="%s">%s</span>', 'mdlr' ), esc_attr($favorite_class ), $favorite_count );
+  //Favorites //////////////////////////////////////
+
+    if(bp_is_user()) {  //again needed this check for some reason but I now forgot what! 
+      bp_core_new_subnav_item( array( 
+      'name' => $favorite_name,
+      'slug' => 'favorites', 
+      'parent_url' => $bp->displayed_user->domain . $parent_slug.'/', 
+      'parent_slug' => $parent_slug, 
+      'position' =>30,
+      'screen_function' => 'mb_author_'.$post_type->name.'_likes' //the function is declared below
+      ) 
+    ); 
+    }//bpisuser
+
+  } //foreach posttype
+
+} //end function
 
 function mdlr_count_cpt_post( $type ) {
   global $wpdb;
@@ -106,27 +86,20 @@ function mdlr_count_cpt_post( $type ) {
   return $wpdb->get_var( "SELECT COUNT(ID) FROM $wpdb->posts WHERE post_author = $user_id AND post_type = '$posttype'  AND post_status = 'publish'" );
 }
 
-//default subnavs don't work with post pagination so we redirect to the sub nav screen
 
-function mdlr_bp_submenu_redirect_product() {
-global $bp;
-bp_core_redirect($bp->displayed_user->domain .'products/added' );
-}
-
-function mdlr_bp_submenu_redirect_event() {
-global $bp;
-bp_core_redirect($bp->displayed_user->domain .'events/added' );
-}
-
-
-/*/////////////////////////////////////////////*/
-/* Setup Profile Screens */
-/*/////////////////////////////////////////////*/
-//here's where things get tricky
 
 function mb_author_post() {
-  add_action( 'bp_template_content', function() { mdlr_get_bp_loop(''.$post_type->name.''); } ); //template ready to for post type slug
+  //get the public post types //////////////////////////////////////
+  add_action( 'bp_template_content', function() { mdlr_get_bp_loop(''.$post_type->name.''); } );
   bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
 }
+
+
+function mb_author_post_likes($type = ''){ 
+  //add_action( 'bp_template_content', 'mb_show_product_likes' );
+  add_action( 'bp_template_content', function() { mdlr_get_bp_loop_likes('product'); } );
+  bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
+}
+
 
 
